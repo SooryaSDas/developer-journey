afcec2f884cade2ea3d9476df4f73bc0beb3228c
Soorya S Das <sooryasandra6@gmail.com>
2026-08-02 20:44:04 +0530

    basics

---
+++ Dayone/Typeof.js
@@ -0,0 +1,12 @@
+name = "soorya"
+age = 28;
+salary = 3.5
+city = "trivandrum"
+isDeveloper = true
+
+console.log("my personal details are : ", name, age, salary, city, isDeveloper);
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(salary));
+console.log(typeof(city));
+console.log(typeof(isDeveloper));
