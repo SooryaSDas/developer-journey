bd0e9f1f8b26c730c7e579528247bd59dce6b809
Soorya S Das <sooryasandra6@gmail.com>
2026-08-04 23:05:09 +0530

    Update execution.js

--- DayTwo/execution.js
+++ DayTwo/execution.js
@@ -18,10 +18,44 @@
 // console.log(a); // 10 20 20
 
 // ---------------------------------------------------------
-var a = 100;
-function test(){
-    var a = 200;
-    console.log(a);
+// var a = 100;
+// function test(){
+//     var a = 200;
+//     console.log(a);
+// }
+// test();
+// console.log(a); // 200 100
+// ---------------------------------------------------------
+
+// var a = 100;
+// function one() {
+//     console.log(a);
+//     a = 200;
+// }
+// one();
+// console.log(a);// 100 200
+// ---------------------------------------------------------
+
+// var a = 10;
+// function one() {
+//     var b = 20;
+//     function two() {
+//         console.log(a);
+//         console.log(b);
+//     }
+//     two();
+// }
+// one();
+
+// ---------------------------------------------------------
+var a = 10;
+function one() {
+    var a = 20;
+    function two() {
+        console.log(a);
+    }
+    two();
 }
-test();
-console.log(a); // 200 100
+one();
+console.log(a); //20 10
+
