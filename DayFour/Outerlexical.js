abce9c367dc2e280929f65774876589ebca04733
Soorya S Das <sooryasandra6@gmail.com>
2026-08-06 22:24:13 +0530

    Create Outerlexical.js

---
+++ DayFour/Outerlexical.js
@@ -0,0 +1,16 @@
+let language = "JavaScript";
+
+function learn() {
+  let topic = "Closure";
+
+  function explain() {
+    console.log(language);
+    console.log(topic);
+  }
+
+  explain();
+}
+learn();
+//  output
+//  JavaScript
+// Closure
