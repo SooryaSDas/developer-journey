923144920e7e048b3591e294879e1886d0f35787
Soorya S Das <sooryasandra6@gmail.com>
2026-08-06 22:34:28 +0530

    Update Outerlexical.js

--- DayFour/Outerlexical.js
+++ DayFour/Outerlexical.js
@@ -1,16 +1,31 @@
-let language = "JavaScript";
+// let language = "JavaScript";
 
-function learn() {
-  let topic = "Closure";
+// function learn() {
+//   let topic = "Closure";
 
-  function explain() {
-    console.log(language);
-    console.log(topic);
-  }
+//   function explain() {
+//     console.log(language);
+//     console.log(topic);
+//   }
 
-  explain();
-}
-learn();
+//   explain();
+// }
+// learn();
 //  output
 //  JavaScript
 // Closure
+
+// ---------------------------------------
+
+function one() {
+
+    function two() {
+        let x = 10;
+    }
+
+    function three() {
+        console.log(x);
+    }
+
+}
+// output : no, Because three()'s Outer Lexical Environment points to one(), not to two().
