955f1992dff75fc5f42bba95d29ec1f03555acc8
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-08-09 23:18:24 +0530

    Update closuresdeep.js

---
+++ DaySix/closuresdeep.js
@@ -0,0 +1,17 @@
+function outer() {
+    let count = 0;
+
+    function inner() {
+        count++;
+        console.log(count);
+    }
+
+    return inner;
+}
+const counter = outer();
+counter(); // 1
+counter(); // 2
+counter(); // 3
+
+// ------------------------------------
+
