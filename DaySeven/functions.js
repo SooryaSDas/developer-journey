691ac5e188c3eee8fdf699ffd98a43ddad88b465
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-08-10 19:04:10 +0530

    argument object

--- DaySeven/functions.js
+++ DaySeven/functions.js
@@ -13,8 +13,17 @@
 
 // -------------------------------------
 // Anonymous Function
-const add = function (a,b){
-    return a+b;
-}
-add(20,10);
+// const add = function (a,b){
+//     return a+b;
+// }
+// add(20,10);
 
+// ------------------------------------
+// "arguments" Object
+
+function add(a,b){
+    console.log(arguments);
+}
+
+add(10,20,30,40) // [Arguments] { '0': 10, '1': 20, '2': 30, '3': 40 }
+// // the arguments object contains all arguments passed to the function. 
