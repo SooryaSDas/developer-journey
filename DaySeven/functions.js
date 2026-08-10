36475038e2c12eacbfd27d2a35ed76af4747f79f
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-08-10 18:41:37 +0530

    Update functions.js

--- DaySeven/functions.js
+++ DaySeven/functions.js
@@ -1,8 +1,14 @@
 // Function Declaration
-function add(num1,num2){
-    return num1+num2;
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// add(10,20); // 30
+
+// -------------------------------------
+// Function Expression - Here the function is created and assigned to a variable.
+const add = function(a,b){
+    return a+b;
 }
-add(10,20); // 30
+add(10, 20);
 
-
-
+// -------------------------------------
