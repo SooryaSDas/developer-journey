83c2eb641971092c54b57a878cd7b25fc7e5a824
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-08-12 09:16:26 +0530

    Update callapplybind.js

--- DayEight/callapplybind.js
+++ DayEight/callapplybind.js
@@ -6,4 +6,15 @@ const value = {
     name : "John"
 }
 
-greet.call(value) // Hello, John
+// greet.call(value) // Hello, John
+
+// ---------------------------------
+// with parameter
+function one(message){
+    console.log(message + ','+ this.name);
+}
+const person = {
+    name : "soorya"
+}
+
+one.call(person, "Hi"); // Hi,soorya
